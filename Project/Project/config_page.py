96a615e7358dfdd556363455b8f1dec0b0b9515e
LenaFedchenko <lenafedcenko98@gmail.com>
2025-12-15 15:09:59 +0200

    added verify for email

--- Project/Project/config_page.py
+++ Project/Project/config_page.py
@@ -1,9 +1,11 @@
 import flask, flask_login
 
-def config_page(name: str):
+def config_page(name: str, url: str):
     def render_template(function: object):
         def processor(*args, **kwargs):
             context = function(*args, **kwargs)
+            if context["message"] == "Successfully":
+                return flask.redirect(url)
             return flask.render_template(
                 template_name_or_list= name,
                 current_user= flask_login.current_user,
--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -1,5 +1,8 @@
-import flask, os
+import flask, os, dotenv
+from .loadenv import ENV_PATH
+from flask_mail import Mail
 
+dotenv.load_dotenv(ENV_PATH)
 
 project = flask.Flask(
     import_name= 'Project',
@@ -8,3 +11,14 @@ project = flask.Flask(
     static_url_path= '/static/',
     instance_path= os.path.abspath(os.path.join(__file__, '..', 'instance'))
 )
+
+project.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+project.config["MAIL_SERVER"] = 'smtp.gmail.com'
+project.config["MAIL_PORT"] = 587
+project.config["MAIL_USE_TLS"] = True
+project.config["MAIL_USE_SSL"] = False
+project.config["MAIL_USERNAME"] = os.environ["MAIL"]
+project.config["MAIL_PASSWORD"] = os.environ["PASSWORD"]
+project.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL"]
+
+mail = Mail(project)
