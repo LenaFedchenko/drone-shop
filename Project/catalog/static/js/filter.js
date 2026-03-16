c0915f21b5164231b7623a20dd75c6bfb3d80137
LenaFedchenko <lenafedcenko98@gmail.com>
2026-03-16 15:08:14 +0200

    fixed pagination

--- Project/catalog/static/js/filter.js
+++ Project/catalog/static/js/filter.js
@@ -1,34 +1,72 @@
 const categoriesObj = document.getElementById("categories")
 const productContainer = document.getElementById("filter_products")
+const paginationContainer = document.getElementById("pagination")
 
-async function filterProducts() {
+async function filterProducts(page) {
     const response = await fetch("/catalog/filter", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            selectCategory: categoriesObj.value
+            selectCategory: categoriesObj.value,
+            page: page
         })
     })
     
-    const request = await response.json()
+    const data = await response.json()
     productContainer.innerHTML = ``
     let html = ``
-    request.filtrated_products.forEach(product => {
+    data.filtrated_products.forEach(product => {
         html += `
-
                 <a href="/catalog/${product.id}" class="product_container">
                     <h1>${product.name}</h1>
                     <img src="/catalog/static/media/${product.image_url}" alt="">
                     <p>${product.description}</p>
-                    <button class="addButton" value="${product.id}">+</button>
-                    <button class="deleteButton" value="${product.id}">-</button>
                 </a>
+                <button class="addButton" value="${product.id}">+</button>
+                <button class="deleteButton" value="${product.id}">-</button>
         `
     });
     productContainer.innerHTML += html
+    renderPaginate(data.pagination)
 }
+
+function renderPaginate(pagination){
+    let paginationHtml = ``
+    if (pagination.has_prev){
+        paginationHtml += `
+        <button class="paginationBtn" value=${pagination.has_prev}>
+            <img src="/catalog/static/images/prev.png" alt="prev">
+        </button>
+        `
+    }
+    for (let page = 1; page <= pagination.total_count; page++) {
+        paginationHtml += `
+            <button class="paginationBtn" value=${page}>${page}</button>
+        `
+    }
+
+    if (pagination.has_next){
+        paginationHtml += `
+        <button class="paginationBtn" value=${pagination.has_next}>
+            <img src="/catalog/static/images/next.png" alt="next">
+        </button>
+        `
+        console.log(pagination.has_next)
+    }
+    paginationContainer.innerHTML = paginationHtml
+    const paginationBtns = document.querySelectorAll(".paginationBtn")
+    paginationBtns.forEach((btn)=>{
+        btn.addEventListener('click', (event)=>{
+            filterProducts(Number(event.currentTarget.value))
+        })
+    })
+}
+
 categoriesObj.addEventListener("change", ()=>{
-    filterProducts()
+    filterProducts(1)
+})
+window.addEventListener("DOMContentLoaded", ()=>{
+    filterProducts(1)
 })
