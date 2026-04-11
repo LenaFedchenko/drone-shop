dfd99a68eee39862fdad55ef484f255f3f7360c9
nz888 <nikon.05.04.2009@gmail.com>
2026-04-11 12:31:23 +0300

    In home page created new products

--- Project/home/views.py
+++ Project/home/views.py
@@ -1,5 +1,8 @@
 import flask
+from Project.db import DATA_BASE
+from catalog.models import Product
+from sqlalchemy import select, desc
 
 def render_home():
-    
-    return flask.render_template("home.html")
+    new_products = products = Product.query.order_by(Product.id.desc()).limit(3).all()
+    return flask.render_template("home.html", products=new_products)
