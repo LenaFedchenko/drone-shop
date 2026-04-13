c033b736fac035c2762512add70520e4c7188ada
LenaFedchenko <lenafedcenko98@gmail.com>
2026-04-13 15:05:53 +0300

    added choose for delivery

--- Project/order/static/js/order.js
+++ Project/order/static/js/order.js
@@ -1,18 +1,37 @@
 const select = document.getElementById("warehouse")
 const cityInput = document.getElementById("cityName")
+const modalForDelivery = document.getElementById("modal_for_delivery")
+const radioBtns = document.getElementsByName("delivery")
 
-async function getWarehouses(city_name) {
-    const response = await fetch(`/order/${city_name}`)
+
+radioBtns.forEach((btn)=>{
+    btn.addEventListener("change", async ()=>{
+        modalForDelivery.style.display = "block"
+        await updateWarehouses()
+        
+    })
+})
+
+async function getWarehouses(city_name, type) {
+    const response = await fetch(`/order/${city_name}?type=${type}`)
     const result = await response.json()
     return result.warehouses
 }
 
 cityInput.addEventListener("change", async (event)=>{
-    const warehouses = await getWarehouses(event.target.value)
-    warehouses.forEach((warehouse) => {
+    await updateWarehouses()
+})
+
+async function updateWarehouses() {
+    const city = cityInput.value
+    const deliveryType = document.querySelector("input[name='delivery']:checked")?.value
+    const warehouses = await getWarehouses(city, deliveryType)
+    select.options.length = 0
+    warehouses.forEach((warehouse)=>{
         const option = document.createElement("option")
         option.textContent = warehouse
         option.value = warehouse
         select.appendChild(option)
-    });
-})
+        
+    })
+}
--- Project/order/views.py
+++ Project/order/views.py
@@ -24,6 +24,8 @@ def render_order():
 
 def get_warehouses(city_name: str):
     TOKEN = os.environ["NOVA_POST_TOKEN"]
+    delivery_type = flask.request.args.get('type')
+    
     payload = {
         "api_key": TOKEN,
         "modelName": "Address",
@@ -39,7 +41,15 @@ def get_warehouses(city_name: str):
     result = response.json()
     warehouses = []
     for data in result["data"]:
-        warehouses.append(data["Description"])
+        # print(data["TypeOfWarehouse"], data["Description"])
+        if data["TypeOfWarehouse"] == "841339c7-591a-42e2-8233-7a0a00f0ed6f" and delivery_type == "warehouse":
+            warehouses.append(data["Description"])
+        elif data["TypeOfWarehouse"] == "f9316480-5f2d-425d-bc2c-ac7cd29decf0" and delivery_type == "parcel_machine":
+            warehouses.append(data["Description"])
+        elif data["TypeOfWarehouse"] == "6f8c7162-4b72-4b0a-88e5-906948c6a92f" and delivery_type == "expres_delivery":
+            warehouses.append(data["Description"])
+        elif data["TypeOfWarehouse"] == "9a68df70-0267-42a8-bb5c-37f427e36ee4" and delivery_type == "courier":
+            warehouses.append(data["Description"])    
     response = flask.make_response(flask.jsonify({
         "warehouses" : warehouses
     }))
@@ -65,6 +75,8 @@ def pay():
         phone = flask.request.form["telephone"]
         email = flask.request.form["email"]
         message = flask.request.form["message"]
+        payment = flask.request.form["payment"]
+
         order = Order(
             first_name= first_name,
             second_name= second_name,
@@ -72,7 +84,8 @@ def pay():
             phone= phone,
             email= email,
             message= message,
-            pay_method = "card"
+            pay_method = payment,
+            warehouse = ""
         )
         for product in product_list:
             order.products.append(product["product"])
@@ -80,20 +93,24 @@ def pay():
         DATA_BASE.session.commit()
     raw_id_list = flask.request.cookies.get("id_list") 
     sum = count_products_price(raw_id_list=raw_id_list)
-    TOKEN = os.environ["MONOBANK_TOKEN"]
-    payload = {
-        "amount": sum * 100,
-        "ccy": 980,
-        "redirectUrl": "http://127.0.0.1:8000"
-    }
-    headers = {
-        "X-Token": TOKEN
-    }
-    response = requests.post(
-        "https://api.monobank.ua/api/merchant/invoice/create",
-        json=payload,
-        headers=headers
-        )
-    request = response.json()
-    pay_url = request["pageUrl"]
-    return flask.redirect(pay_url)
+
+    if payment == "card":
+        TOKEN = os.environ["MONOBANK_TOKEN"]
+        payload = {
+            "amount": sum * 100,
+            "ccy": 980,
+            "redirectUrl": "http://127.0.0.1:8000"
+        }
+        headers = {
+            "X-Token": TOKEN
+        }
+        response = requests.post(
+            "https://api.monobank.ua/api/merchant/invoice/create",
+            json=payload,
+            headers=headers
+            )
+        request = response.json()
+        pay_url = request["pageUrl"]
+        return flask.redirect(pay_url)
+    
+    return flask.redirect("/")
