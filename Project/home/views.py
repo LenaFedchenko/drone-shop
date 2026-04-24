d2cd948a170857af8be9f8b75512a8b0ecfabcdc
nz888 <nikon.05.04.2009@gmail.com>
2026-04-24 10:47:27 +0300

    ...

--- Project/home/views.py
+++ Project/home/views.py
@@ -1,5 +1,5 @@
 import flask
-
+from catalog.models import Product
 def render_home():
-    
-    return flask.render_template("home.html")
+    new_products = Product.query.order_by(Product.id.desc()).limit(3).all()
+    return flask.render_template("home.html", new_products=new_products)
