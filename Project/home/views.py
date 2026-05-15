2264cecb582871548543a0dde0db3815121c30ac
nz888 <nikon.05.04.2009@gmail.com>
2026-05-15 22:20:31 +0300

    home page update

--- Project/home/views.py
+++ Project/home/views.py
@@ -2,4 +2,5 @@ import flask
 from catalog.models import Product
 def render_home():
     new_products = Product.query.order_by(Product.id.desc()).limit(3).all()
-    return flask.render_template("home.html", new_products=new_products)
+    catalog = Product.query.limit(4)
+    return flask.render_template("home.html", new_products=new_products, catalog=catalog)
